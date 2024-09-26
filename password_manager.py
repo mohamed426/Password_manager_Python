7cb930229442b6b1b6aaafcae22efb48291d5364
Mo Khaled <58119633+mohamed426@users.noreply.github.com>
2024-09-27 00:48:59 +0300

    password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -24,7 +24,7 @@ def main():
         try:
             # Display the menu options.
             print("1. Generate Password")
-            print("2. Save Password")
+            print("2. Add Password")
             print("3. Delete Password")
             print("4. Update Password")
             print("5. Show Passwords")
@@ -35,9 +35,11 @@ def main():
 
             # Call the corresponding function based on user input.
             if operation == 1:
-                print(f"Generated Password: {generate_password()}", end='\n\n')
+                generated_pass = generate_password()
+                print(f"Generated Password: {generated_pass}", end='\n\n')
+                save_generated(generated_pass)
             elif operation == 2:
-                save_password()
+                add_password()
             elif operation == 3:
                 delete_password()
             elif operation == 4:
@@ -70,16 +72,16 @@ def generate_password():
     try:
         # Get user preferences for including different character types in the password.
         password = []
-        nums = input("- Include numbers? [yes, no]: ").lower()
+        nums = input("- Include numbers? [yes, no]: ").lower().strip()
         if nums not in ["yes", "y", "no", "n"]:
             raise ValueError
-        lows = input("- Include small letters? [yes, no]: ").lower()
+        lows = input("- Include small letters? [yes, no]: ").lower().strip()
         if lows not in ["yes", "y", "no", "n"]:
             raise ValueError
-        caps = input("- Include capital letters? [yes, no]: ").lower()
+        caps = input("- Include capital letters? [yes, no]: ").lower().strip()
         if caps not in ["yes", "y", "no", "n"]:
             raise ValueError
-        puncs = input("- Include special characters? [yes, no]: ").lower()
+        puncs = input("- Include special characters? [yes, no]: ").lower().strip()
         if puncs not in ["yes", "y", "no", "n"]:
             raise ValueError
 
@@ -103,14 +105,14 @@ def generate_password():
         # Generate the password using the selected character types.
         for _ in range(length):
             password.append(random.choice(pass_contain))
-            
+        
         return ''.join(password)  # Return the generated password as a string.
     except ValueError:
         # Handle invalid input for character type inclusion.
         print("Please enter yes or no", end="\n\n")
         return generate_password()
 
-def save_password():
+def add_password():
     """
     Function to save a password in the database.
     """
@@ -129,12 +131,17 @@ def save_password():
     except ValueError:
         # Handle invalid input for password name.
         print("Please enter what the password is for. ex: facebook, instagram")
-        return save_password()
+        return add_password()
 
 def show_passwords():
     """
     Function to display saved passwords based on user preference.
     """
+    data = db.execute("SELECT * FROM passwords")
+    if len(data) == 0:
+        print("There are no data to display it", end="\n\n")
+        return
+    
     try:
         # Get user choice for showing specific, all, or no passwords.
         show = int(input("1. Show Specific Password\n2. Show All Passwords\n3. Back to main\n"))
@@ -148,11 +155,7 @@ def show_passwords():
                 print(tabulate(data, headers="keys", tablefmt="grid"), end="\n\n")
         elif show == 2:
             # Show all saved passwords.
-            data = db.execute("SELECT * FROM passwords")
-            if len(data) == 0:
-                print("There are no data to display it", end="\n\n")
-            else:
-                print(tabulate(data, headers="keys", tablefmt="grid"), end="\n\n")
+            print(tabulate(data, headers="keys", tablefmt="grid"), end="\n\n")
         elif show == 3:
             # Go back to the main menu.
             return
@@ -167,6 +170,11 @@ def delete_password():
     """
     Function to delete passwords from the database.
     """
+    search_for_data = db.execute("SELECT * FROM passwords")
+    if len(search_for_data) == 0:
+        print("There are no data to delete it", end="\n\n")
+        return 
+    
     try:
         # Get user choice for deleting specific, all, or no passwords.
         delete = int(input("1. Delete Specific Password\n2. Delete All Passwords\n3. Back to main\n"))
@@ -184,13 +192,9 @@ def delete_password():
                 db.execute("DELETE FROM passwords WHERE password_name LIKE (?)", pass_name)
                 print(f"{pass_name.title()} Deleted Successfully",end="\n\n")
         elif delete == 2:
-            # Delete all saved passwords.
-            search_for_data = db.execute("SELECT * FROM passwords")
-            if len(search_for_data) == 0:
-                print("There are no data to delete it", end="\n\n")
-            else:
-                db.execute("DELETE FROM passwords")
-                print("All Data Deleted Successfully", end="\n\n")
+            # Delete all saved passwords.    
+            db.execute("DELETE FROM passwords")
+            print("All Data Deleted Successfully", end="\n\n")
         elif delete == 3:
             # Go back to the main menu.
             return
@@ -205,6 +209,10 @@ def update_password():
     """
     Function to update an existing password in the database.
     """
+    search_for_pass = db.execute("SELECT * FROM passwords")
+    if len(search_for_pass) == 0:
+        print("No data to update it", end="\n\n")
+        return
     # Get the name associated with the password to be updated.
     pass_name = input("Password name: ")
     if not pass_name:
@@ -223,6 +231,27 @@ def update_password():
 
         db.execute("UPDATE passwords SET password = (?) WHERE password_name LIKE (?)", new_pass, pass_name)
         print(f"{pass_name.title()} Updated Successfully", end="\n\n")
+
+def save_generated(generated_pass):
+    try:
+        save = input("Do you want to save it? [yes, no]").lower().strip()
+        if save in ["y", "yes"]:
+            while True:
+                pass_name = input("Password for: ").strip()
+                if not pass_name.isalpha():
+                    print("Please enter what the password is for. ex: facebook, instagram")
+                else:
+                    break
+            db.execute("INSERT INTO passwords VALUES(?, ?)", pass_name, generated_pass)
+            print("Password Saved", end="\n\n")
+        elif save in ["n", "no"]:
+            return
+        else:
+            raise ValueError
+    except ValueError:
+        print("Please enter yes or no", end="\n\n")
+        return save_generated(generated_pass)
+    
 
 if __name__ == "__main__":
     # Run the main function when the script is executed.
